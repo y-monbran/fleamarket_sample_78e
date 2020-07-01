78515eb5c14083748fe6b3675d07410bd4bf4bc0
y-monbran <gohangaoisiiyo@gmail.com>
2020-07-01 16:55:42 +0900

    change_file_2times

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:y-monbran/fleamarket_sample_78e.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.6.5'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/chatspace.pem']
