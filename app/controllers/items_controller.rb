b959c9338bbd28d2a50dae3b05cf15a65260939f
y-monbran <gohangaoisiiyo@gmail.com>
2020-07-08 19:29:49 +0900

    add_pullrequest

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,51 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
+
   def index
+    @items = Item.includes(:item_imgs).order('created_at DESC') # includes(:images)だった
   end
+
   def show
     @item = Item.find(params[:id])
   end
 
+  def new
+    @item = Item.new
+    @item.item_img.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, images_attributes: [:src])
+  end
+
+  def set_product
+    @item = Item.find(params[:id])
+  end
 end
