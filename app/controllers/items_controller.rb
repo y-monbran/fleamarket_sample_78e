b56c078e975995a19044f75a9f92a12664dd146d
yuuichi1300 <reyouth1300@gmail.com>
2020-07-15 12:28:04 +0900

    item_destroy_kobayashi

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,12 @@ class ItemsController < ApplicationController
   def show
     @items = Item.find(params[:id])
   end
+  # def destroy
+  #   item = Item.find(params[:id])
+  #    if item.user_id == current_user.id
+  #     item.destroy
+  #    end
+  #  end
 
   private
 
